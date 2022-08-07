ef1aee0c1055a70cf61963c3675b06c96fedcecd
Shikhar Rai <34543293+kakashi215@users.noreply.github.com>
2022-08-07 10:32:04 +0530

    Added AnimeTrack model

--- AnimeWebsite/WatchTracker/models.py
+++ AnimeWebsite/WatchTracker/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
+
+class AnimeTrack(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    episode_count = models.IntegerField(
+        blank=False,
+        default=1,
+        validators=[MinValueValidator(1), MaxValueValidator(9999)],
+    )
+    watched = models.IntegerField(
+        blank=False,
+        default=0,
+        validators=[MinValueValidator(0), MaxValueValidator(9999)],
+    )
+    added_on = models.DateTimeField(auto_now_add=True)
+
+    def __repr__(self):
+        return (
+            self.title
+            + " added by "
+            + self.user
+            + " on "
+            + self.added_on
+            + ". Watched "
+            + self.watched
+            + "/"
+            + self.episode_count
+            + "."
+        )
