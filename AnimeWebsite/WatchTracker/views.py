5568083fa228ef8cb9d9d913cff76bb63dbfec49
Shikhar Rai <34543293+kakashi215@users.noreply.github.com>
2022-08-07 10:10:33 +0530

    Login required for home.

--- AnimeWebsite/WatchTracker/views.py
+++ AnimeWebsite/WatchTracker/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # from django.contrib.auth.forms import UserCreationForm as RegistrationForm
 from .forms import RegistrationForm 
 
-
+@login_required(login_url='/watchTracker/login')
 def home(request):
     return render(request, 'home.html')
 
