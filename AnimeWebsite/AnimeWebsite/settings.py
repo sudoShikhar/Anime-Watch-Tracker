0122f33c824efcb4a7b05b9584aa15b53beac549
kakashi215 <34543293+kakashi215@users.noreply.github.com>
2024-05-10 14:02:35 +0530

    static files

--- AnimeWebsite/AnimeWebsite/settings.py
+++ AnimeWebsite/AnimeWebsite/settings.py
@@ -40,8 +40,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "WatchTracker",
-    "crispy_forms",
-    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -120,12 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-LOGIN_REDIRECT_URL = "/watchTracker/"
-LOGOUT_REDIRECT_URL = "/watchTracker/login"
+LOGIN_URL = "/watchTracker/login"
+LOGIN_REDIRECT_URL = "/watchTracker/home"
+LOGOUT_REDIRECT_URL = "/watchTracker/home"
